c6d9f32775c2b97b563dc05b8c9c5927d2e90a5d
Khris Punzalan <khrispunzalan@gmail.com>
2019-06-22 17:24:25 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,45 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def count
+    @@count
+  end
+
+  def genres
+    @@genres.uniq!
+
+  end
+
+  def artists
+    @@artist.uniq!
+  end
+
+  def genre_count
+    binding.pry 
+    hash = []
+    hash[@@genres] = @@count
+  end
+    hash
+  end
+
+  def artist_count
+
+  end
+
+
+end
